538c52cba1c2252cc8c96a1be03611af2e9b699d
Brucy William <brucywlalmeida@icloud.com>
2022-09-14 16:53:16 -0300

    adcionar função de login

---
+++ script.js
@@ -0,0 +1,13 @@
+const trybeLogin = document.getElementById('trybeLogin');
+
+function validation() {
+  const login = document.getElementById('login').value;
+  const password = document.getElementById('password').value;
+  if (login === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+trybeLogin.addEventListener('click', validation);
